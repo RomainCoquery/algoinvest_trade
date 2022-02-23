de04c8b8e3c7d836cc6e5534238c918a20c6b81a
Romain Coquery <romaincoquery@gmail.com>
2022-02-23 20:31:30 +0100

    Amélioration Bruteforce, Greedy et Dynamic

--- bruteforce.py
+++ bruteforce.py
@@ -8,62 +8,53 @@ BUDGET = 500
 
 def create_list(file):
     file = open(sys.argv[1])
-    csvreader = csv.reader(file)
-    header = next(csvreader)
-    actions = []
-    incorrect_lines = 0
-    for action in csvreader:
-        if float(action[1]) > 0.0 and float(action[2]) > 0.0:
-            actions.append(action)
-        else:
-            incorrect_lines += 1
+    reader = csv.reader(file)
+    header = next(reader)
+    actions = [[r[0], float(r[1]), float(r[1]) * float(r[2]) / 100] for r in reader]
     file.close()
-    return header, actions, incorrect_lines
 
+    return header, actions
 
 def bruteforce(dataset):
     best_profit = 0
     best_comb = None
+    expense = 0
 
     for i in range(len(dataset)):
         for comb in combinations(dataset, i + 1):
-            price = sum([float(i[1]) for i in comb])
-            profit = sum([float(i[1]) * float(i[2]) / 100 for i in comb])
+            price = sum([i[1] for i in comb])
+            profit = sum([i[2]for i in comb])
             if price <= BUDGET and profit > best_profit:
                 best_profit = profit
                 best_comb = comb
+                expense = price
 
-    total_profit = sum(float(i[1]) for i in best_comb)
-    return best_profit, best_comb, total_profit
-
+    return best_profit, best_comb, expense
 
 def main():
-    start_time = time()
-
     if len(sys.argv) < 2:
         print("You must specify a file.")
     elif len(sys.argv) != 2:
         print("There are too many arguments specified.")
     else:
         try:
+            start_time = time()
             print("\nLoading data...\n")
-            header, actions, incorrect_lines = create_list(sys.argv[1])
-            print(f"Deletion of {incorrect_lines} incorrect lines")
+            header, actions = create_list(sys.argv[1])
         except FileNotFoundError:
             print(f"No such file or directory: '{sys.argv[1]}'\n")
 
     print("\nBruteforce working, please wait...\n")
     print(f"  {header[0]}        {header[1]}        {header[2]}\n")
-    best_profit, best_comb, total_profit = bruteforce(actions)
+    best_profit, best_comb, expense = bruteforce(actions)
     for i in best_comb:
         print(f"{str(i[0])} for {round(float(i[1]), 2)}€ and "
-        f"{round(float(i[1]) * float(i[2]) / 100, 2)}€ of profit.")
+        f"{round(float(i[2]), 2)}€ of profit.")
     print(f"\nTotal profit is : {round(best_profit, 2)}€ for a budget of"
-    f" {round(total_profit, 2)}€.")
+    f" {round(expense, 2)}€.")
 
     execution_time = round(time() - start_time, 4)
     print(f"\nExecution time: {execution_time}s\n")
-
 
 if __name__ == "__main__":
     main()
--- optimized.py
+++ optimized.py
@@ -3,95 +3,137 @@ import csv
 from time import time
 
 BUDGET = 500
+NAME, PRICE, PROFIT_PERCENT, PROFIT_EURO = 0, 1, 2, 3
 
 
 def create_list(file):
+    rows, total_lines, incorrect_lines = [], 0, 0
+
     file = open(sys.argv[1])
     csvreader = csv.reader(file)
     header = next(csvreader)
-    actions = []
-    incorrect_lines = 0
-    for action in csvreader:
-        if float(action[1]) > 0.0 and float(action[2]) > 0.0:
-            actions.append(action)
+    for row in csvreader:
+        total_lines += 1
+        if float(row[1]) > 0 and float(row[2]) > 0:
+            rows.append(
+                [
+                    row[NAME], 
+                    float(row[PRICE]), 
+                    float(row[PROFIT_PERCENT]), 
+                    float(row[PRICE]) * float(row[PROFIT_PERCENT]) / 100
+                ]
+            )
         else:
             incorrect_lines += 1
     file.close()
-    return header, actions, incorrect_lines
+
+    return header, rows, total_lines, incorrect_lines
+
+def float_to_int(data):
+    return [
+        (
+            d[NAME], 
+            round(d[PRICE] * 100), 
+            round(d[PROFIT_PERCENT] * 100), 
+            round(d[PROFIT_EURO] * 100)
+        ) 
+        for d in data
+        ]
+
+def int_to_float(data):
+    return [
+        (
+            d[NAME], 
+            round((d[PRICE] / 100), 2), 
+            round((d[PROFIT_PERCENT] / 100), 2), 
+            round((d[PROFIT_EURO] / 100), 2)
+        ) 
+            for d in data
+        ]
 
 def greedy(dataset):
-    actions = sorted(dataset, key=lambda x: float(x[2]) / 100)
-    actions_selected = []
-    expense = 0
+    actions = sorted(dataset, key=lambda x: x[PROFIT_PERCENT])
+    actions_selected, expense = [], 0
 
     while actions:
         action = actions.pop()
-        if float(action[1]) + expense < BUDGET:
+        if action[PRICE] + expense < BUDGET:
             actions_selected.append(action)
-            expense += float(action[1])
+            expense += action[PRICE]
 
-    total_profit = sum(float(i[1]) * float(i[2]) / 100 for i in actions_selected)
-    return actions_selected, expense, total_profit
-
+    return actions_selected
 
 def dynamic(dataset):
-    actions = sorted(dataset, key=lambda x: float(x[1]))
+    actions = float_to_int(dataset)
     actions_selected = []
-    expense = 0
-    matrix = [[0 for x in range(BUDGET + 1)] for x in range(len(actions) + 1)]
-
-    for i in range(1, len(actions) + 1):
-        for w in range(1, BUDGET + 1):
-            if float(actions[i - 1][1]) <= w:
-                matrix[i][w] = max(int(actions[i - 1][2]) + matrix[i - 1]
-                [w - int(actions[i - 1][1])], matrix[i - 1][w])
-    
-    w = BUDGET
+    w = BUDGET * 100
     n = len(actions)
 
-    while w > 0 and n > 0:
-        action = actions[n - 1]
-        if matrix[n][w] == matrix[n - 1][w - int(action[1])] + int(action[2]):
+    matrix = [[0 for x in range(w + 1)] for x in range(n + 1)]
+
+    for i in range(1, n + 1):
+        for w in range(1, w + 1):
+            if actions[i-1][PRICE] <= w:
+                matrix[i][w] = max(
+                            actions[i-1][PROFIT_EURO] + 
+                            matrix[i-1][w-actions[i-1][PRICE]], matrix[i-1][w]
+                        )
+            else:
+                matrix[i][w] = matrix[i-1][w]
+
+    while w >= 0 and n >= 0:
+        action = actions[n-1]
+        if matrix[n][w] == matrix[n-1][w-action[PRICE]] + action[PROFIT_EURO]:
             actions_selected.append(action)
-            w -= int(action[1])
+            w -= action[PRICE]
         n -= 1
     
-    for action in actions_selected:
-        expense += float(action[1])
+    return int_to_float(actions_selected)
 
-    total_profit = sum(float(i[1]) * float(i[2]) / 100 for i in actions_selected)
-    return actions_selected, expense, total_profit
+def choice(data):
+    choice = None
+    while not choice:
+        choice = input("Please choose an algorithm:\n\n"
+                        "[1] Greedy (faster)\n"
+                        "[2] Dynamic (better)\n\n"
+                        ">> "
+                        )
 
+        if choice == '1':
+            algorithm = greedy(data)
+        elif choice == '2':
+            algorithm = dynamic(data)
+
+    return algorithm
 
 def main():
-    start_time = time()
-
     if len(sys.argv) < 2:
         print("You must specify a file.")
     elif len(sys.argv) != 2:
         print("There are too many arguments specified.")
     else:
         try:
-            print("\nLoading data...\n")
-            header, actions, incorrect_lines = create_list(sys.argv[1])
-            print(f"Deletion of {incorrect_lines} incorrect lines")
+            header, rows, total_lines, incorrect_lines = create_list(sys.argv[1])
+            start_time = time()
+            actions_selected = choice(rows)
         except FileNotFoundError:
             print(f"No such file or directory: '{sys.argv[1]}'\n")
 
-    print("\nOptimized working, please wait...\n")
-    print(f"  {header[0]}        {header[1]}        {header[2]}\n")
-    
-    actions_selected, expense, total_profit, = greedy(actions)
+    print(f"\nAnalyzing {total_lines} lines in document\n\n"
+            f"Deletion of {incorrect_lines} incorrect lines\n\n"
+            f"   {header[0]}       {header[1]}      {header[2]}\n")
+
     for i in actions_selected:
-        print(f"{i[0]} for {round(float(i[1]), 2)}€ and "
-        f"{round(float(i[1]) * float(i[2]) / 100, 2)}€ of profit.")
-    
-    print(f"\nTotal profit is : {round(total_profit, 2)}€ for a budget of"
-    f" {round(expense, 2)}€.")
+        print(f"{i[NAME]} for {round(i[PRICE], 2)}€ and "
+                f"{round(i[PROFIT_EURO], 2)}€ of profit.")
+
+    print(
+    f"\nTotal profit is : {round(sum(i[PROFIT_EURO] for i in actions_selected), 2)}"
+    f"€ for a budget of {round(sum(i[PRICE] for i in actions_selected), 2)}€."
+    )
     
     execution_time = round(time() - start_time, 4)
     print(f"\nExecution time: {execution_time}s\n")
 
-
-if __name__ == "__main__":
+if __name__ in "__main__":
     main()
